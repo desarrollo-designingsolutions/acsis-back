da33fb85fc12cdbce0bc3a5ad5a812ff365a3ed6
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-04-30 07:38:57 -0500

    first commit

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Country\CountrySelectResource;
+use App\Repositories\CityRepository;
+use App\Repositories\CountryRepository;
+use App\Repositories\StateRepository;
+use App\Repositories\UserRepository;
+use App\Traits\HttpResponseTrait;
+use Illuminate\Http\Request;
+
+class QueryController extends Controller
+{
+    use HttpResponseTrait;
+
+    public function __construct(
+        protected CountryRepository $countryRepository,
+        protected StateRepository $stateRepository,
+        protected CityRepository $cityRepository,
+        protected UserRepository $userRepository,
+    ) {}
+
+    public function selectInfiniteCountries(Request $request)
+    {
+        return $this->execute(function () use ($request) {
+            $countries = $this->countryRepository->list($request->all());
+
+            $dataCountries = CountrySelectResource::collection($countries);
+
+            return [
+                'countries_arrayInfo' => $dataCountries,
+                'countries_countLinks' => $countries->lastPage(),
+            ];
+        });
+    }
+
+    public function selectStates($country_id)
+    {
+        return $this->execute(function () use ($country_id) {
+            $states = $this->stateRepository->selectList($country_id);
+
+            return [
+                'code' => 200,
+                'states' => $states,
+            ];
+        });
+    }
+
+    public function selectCities($state_id)
+    {
+        return $this->execute(function () use ($state_id) {
+            $cities = $this->cityRepository->selectList($state_id);
+
+            return [
+                'code' => 200,
+                'cities' => $cities,
+            ];
+        });
+    }
+
+    public function selectCitiesCountry($country_id)
+    {
+        return $this->execute(function () use ($country_id) {
+            $country = $this->countryRepository->find($country_id, ['cities']);
+
+            return [
+                'code' => 200,
+                'message' => 'Datos Encontrados',
+                'cities' => $country['cities']->map(function ($item) {
+                    return [
+                        'value' => $item->id,
+                        'title' => $item->name,
+                    ];
+                }),
+            ];
+        });
+    }
+}
