0ac90dbaf5aa93ed23ce37b448096ba63f8a5e71
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-05-12 14:19:38 -0500

    cambios

--- app/Http/Resources/Invoice/InvoiceListResource.php
+++ app/Http/Resources/Invoice/InvoiceListResource.php
@@ -19,13 +19,13 @@ class InvoiceListResource extends JsonResource
             'entity_name' => $this->entity?->corporate_name,
             'invoice_number' => $this->invoice_number,
             'type' => $this->type,
-            'type_description' =>  $this->type->description(),
+            'type_description' =>  $this->type?->description(),
             'value_paid' => formatNumber($this->value_paid),
             'value_glosa' => formatNumber($this->value_glosa),
             'radication_date' => $this->radication_date,
             'patient_name' => $this->patient?->full_name,
             'status' => $this->status,
-            'status_description' => $this->status->description(),
+            'status_description' => $this->status?->description(),
         ];
     }
 }
