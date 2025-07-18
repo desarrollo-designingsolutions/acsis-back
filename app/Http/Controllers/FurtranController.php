7c1ef690d40e0d0787e1ee9dd273f1a1555d6365
Juan Naranjo <juanesnc16@gmail.com>
2025-07-18 15:54:25 -0500

    Cambios

--- app/Http/Controllers/FurtranController.php
+++ app/Http/Controllers/FurtranController.php
@@ -51,12 +51,24 @@ class FurtranController extends Controller
     {
         return $this->execute(function () use ($invoice_id) {
 
-            $invoice = $this->invoiceRepository->find($invoice_id, with: ['typeable:id,insurance_statuse_id', 'typeable.insurance_statuse:id,code'], select: ['id', 'type', 'typeable_type', 'typeable_id']);
+            $invoice = $this->invoiceRepository->find($invoice_id, with: [
+                'typeable:id,insurance_statuse_id',
+                'typeable.insurance_statuse:id,code',
+                'serviceVendor:id,ipsable_type,ipsable_id',
+                'serviceVendor.ipsable:id,codigo',
+            ], select: [
+                'id',
+                'type',
+                'typeable_type',
+                'typeable_id',
+                'service_vendor_id',
+            ]);
             $invoice = [
                 'id' => $invoice->id,
                 'insurance_statuse_code' => $invoice->typeable?->insurance_statuse?->code,
-                'cod_habilitacion' => $invoice?->serviceVendor?->ipsable?->codigo,
+                'serviceVendor_ipsable_codigo' => $invoice?->serviceVendor?->ipsable?->codigo,
             ];
+
 
             $rgResponseEnum = $this->queryController->selectRgResponseEnum(request());
             $genderEnum = $this->queryController->selectGenderEnum(request());
